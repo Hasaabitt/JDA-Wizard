1d38da46ba3a98aaf362a288cef9df2436abc4cc
GitHub Action <action@github.com>
2022-01-03 08:05:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/hasaabitt/jdawizard/MyBundle.kt
+++ src/main/kotlin/com/github/hasaabitt/jdawizard/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hasaabitt.jdawizard
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hasaabitt/jdawizard/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/hasaabitt/jdawizard/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.hasaabitt.jdawizard.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hasaabitt.jdawizard.MyBundle
 
 class MyApplicationService {
 
